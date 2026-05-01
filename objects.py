3230fe811fd5f5f75a131292308768d330e0d50c
Bolimart <157001545+Bolimart@users.noreply.github.com>
2026-05-01 10:09:37 +0200

    Update line render

--- objects.py
+++ objects.py
@@ -21,3 +21,5 @@ class triangle:
         self.L0 = Line(P0, P1)
         self.L1 = Line(P1, P2)
         self.L2 = Line(P2, P0)
+        
+    def is_point_inside(self, P):
--- renderer.py
+++ renderer.py
@@ -12,7 +12,7 @@ def lerp(i0, d0, i1, d1):
     values = []
     a = (d1 - d0) / (i1 - i0)
     d = d0
-    for i in range(i0, i1):
+    for i in range(i0, i1 + 1):
         values.append(round(d))
         d = d + a
     return values
@@ -29,7 +29,7 @@ def draw_line(line, color, image: np.array):
             x0, x1 = swap(x0, x1)
             y0, y1 = swap(y0, y1)
         ys = lerp(x0, y0, x1, y1)
-        for x in range(x0, x1):
+        for x in range(x0, x1 + 1):
             image[ys[x - x0], x] = color
             
     # The line is vertical-ish
@@ -40,19 +40,21 @@ def draw_line(line, color, image: np.array):
             x0, x1 = swap(x0, x1)
             y0, y1 = swap(y0, y1)
         xs = lerp(y0, x0, y1, x1)
-        for y in range(y0, y1):
+        for y in range(y0, y1 + 1):
             image[y, xs[y - y0]] = color    
     
 
 # =====[ Render ]=====
 
 def render(viewport, width, height, debug=False):
-    P1, P2 = Point2D(0, 0), Point2D(10, 40)
-    P3, P4 = Point2D(0, 0), Point2D(40, 10)
+    P1, P2 = Point2D(0, 0), Point2D(10, 39)
+    P3, P4 = Point2D(0, 0), Point2D(39, 10)
     image = np.zeros([height, width, 3])
 
-    draw_line(Line(P1, P2), np.array([1, 1, 1]), image)
-    draw_line(Line(P3, P4), np.array([1, 1, 1]), image)
+    
+    draw_line(Line(P1, P2), np.array([1, 0, 0]), image)
+    draw_line(Line(P2, P4), np.array([0, 1, 0]), image)
+    draw_line(Line(P3, P4), np.array([0, 0, 1]), image)
     
     return image
 
